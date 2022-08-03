27e9686046dec18f1c545a57464d680002238bcc
DarkWob <71833890+darkwob@users.noreply.github.com>
2022-08-03 19:42:31 +0300

    Update sayitahmin.php

--- sayitahmin.php
+++ sayitahmin.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>SAYI TAHMİN OYUNU- PHP DERS(BENİM YAPTIM)</title>
+    <title>SAYI TAHMİN OYUNU</title>
 
 </head>
 <body>
